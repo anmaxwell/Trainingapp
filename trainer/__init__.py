6938bc8e9ccf2bc05cca2f346b3c1e78b4b4c7ce
anmaxwell <aniamaxwell@gmail.com>
2020-05-28 18:09:54 +0100

    created login form and started signup form

--- trainer/__init__.py
+++ trainer/__init__.py
@@ -2,7 +2,12 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
+#adding security to prevent XS forgery attacks etc.
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
+
 
 #creating the db in the same directory
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
--- trainer/routes.py
+++ trainer/routes.py
@@ -1,8 +1,20 @@
 from trainer import app
+from trainer.forms import SignUpForm, LoginForm
 from flask import render_template, url_for
 
 @app.route('/')
 def index():
     return render_template('home.html')
-    #return("Hello World!")
     
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    form = SignUpForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('signup.html', title='SignUp', form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form)
