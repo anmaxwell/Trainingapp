1036a685c85954671bad2ed64be53599945e2170
anmaxwell <aniamaxwell@gmail.com>
2020-06-14 14:24:51 +0100

    created database and tables for users and training

--- trainer/__init__.py
+++ trainer/__init__.py
@@ -4,14 +4,15 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+
 #adding security to prevent XS forgery attacks etc.
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 
 
-#creating the db in the same directory
+#creating the db in the same directory using ///
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
 db = SQLAlchemy(app)
 
 #to prevent getting stuck in circular imports create this last
-from trainer import routes
+from trainer import routes, models
--- trainer/models.py
+++ trainer/models.py
@@ -1,2 +1,23 @@
+from datetime import datetime
 from trainer import db
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    training = db.relationship('Training', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
+class Training(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    provider = db.Column(db.String(60), nullable=False)
+    title = db.Column(db.String(60), nullable=False)
+    date_taken = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    review = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.provider}', '{self.title}')"
