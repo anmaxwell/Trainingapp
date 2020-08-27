7d18b17d9991433ee0e94d05cd1f230c3af25296
anmaxwell <aniamaxwell@gmail.com>
2020-08-27 21:54:25 +0100

    updated log training screen to have drop down for rating and calendar drop down for date field

--- trainer/forms.py
+++ trainer/forms.py
@@ -5,7 +5,6 @@ from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 from trainer.models import User, Training
 
-
 class SignUpForm(FlaskForm):
     email = StringField('Email',
                         validators=[DataRequired(), Email(), Length(min=2, max=120)])
@@ -29,13 +28,13 @@ class Profile(FlaskForm):
                     ,(4,"Mid"),(5,"Senior"),(6,"Principal")], coerce=int)
     submit = SubmitField('Login')
 
-
 class LogTraining(FlaskForm):
     provider = StringField('Provider',
                            validators=[DataRequired(), Length(max=20)])
     title = StringField('Title',
                            validators=[DataRequired(), Length(min=2, max=120)])
     date_taken = DateField('Date Entered', format='%Y-%m-%d')
-    rating = SelectField(u'Rating', choices=[], coerce=int)
+    rating = SelectField(u'Rating', choices=[(1,"Definitely wouldn't recommend"),(2,"Wouldn't recommend"),(3,"Neutral")
+                    ,(4,"Would recommend"),(5,"Definitely would recommend")], coerce=int)
     review = TextAreaField('Review')
     submit = SubmitField('Login')
