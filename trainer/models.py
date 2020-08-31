02475aa6d97c6329e7193edbd8e8db7a05bf5afa
anmaxwell <aniamaxwell@gmail.com>
2020-08-31 17:56:35 +0100

    comment out training to test db

--- trainer/models.py
+++ trainer/models.py
@@ -4,7 +4,7 @@ from trainer import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    training = db.Column(db.Integer, db.ForeignKey('training.id'), nullable=False)
+    #training = db.Column(db.Integer, db.ForeignKey('training.id'), nullable=False)
     role = db.Column(db.Integer, db.ForeignKey('role.id'), nullable=False)
     level = db.Column(db.Integer, db.ForeignKey('level.id'), nullable=False)
 
@@ -13,14 +13,14 @@ class User(db.Model):
 
 class Training(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    provider = db.Column(db.String(60), nullable=False)
-    title = db.Column(db.String(60), nullable=False)
-    date_taken = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    review = db.Column(db.Text)
-    user_id = db.relationship('User', backref='usertrain', lazy=True)
+ #   provider = db.Column(db.String(60), nullable=False)
+ #   title = db.Column(db.String(60), nullable=False)
+ #   date_taken = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+ #   review = db.Column(db.Text)
+ #   user_id = db.relationship('User', backref='usertrain', lazy=True)
 
-    def __repr__(self):
-        return f"User('{self.provider}', '{self.title}')"
+ #   def __repr__(self):
+ #       return f"User('{self.provider}', '{self.title}')"
 
 class Role(db.Model):
     id = db.Column(db.Integer, primary_key=True)
